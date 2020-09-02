96b1f186ca46ec4a35ebee8d2887ee4a60e2fdea
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-09-02 21:59:36 +0545

    Add highlighter js outline

---
+++ glossary-highlighter.js
@@ -0,0 +1,14 @@
+/**
+ * glossary-highlighter.js
+ * A Simple glossary/word highlighter plugin
+ * 2020-09-02 - version 1.0.0
+ * https://github.com/gorkhali125/glossary-highlighter/blob/master/glossary-highlighter.js
+ * 
+ * Copyright 2020 Deepesh khanal
+ * Release under MIT License
+ * https://opensource.org/licenses/MIT
+ */
+
+(function () {
+
+})();
