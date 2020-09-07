4a9788025a7b949f892f9b070c3794272e2fabc8
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-09-07 21:57:39 +0545

    Added some real words meaning

--- plugin/glossary-highlighter.js
+++ plugin/glossary-highlighter.js
@@ -15,7 +15,19 @@
         };
 
         var highlightText = jQuery('.body');
-        var glossaries = { 'matters': 'A substance', 'hungry': 'Empty Stomach', 'rumbling': 'Gudung Gudung', 'began': 'Started' };
+        var glossaries = {
+            'hungry': 'feeling or showing the need for food',
+            'rumbling': 'a continuous deep, resonant sound',
+            'aardvark': 'a nocturnal badger-sized burrowing mammal of Africa, with long ears, a tubular snout, and a long extensible tongue, feeding on ants and termites',
+            'pinafore': 'a collarless sleeveless dress worn over a blouse or jumper',
+            'humbugs': 'deceptive or false talk or behaviour',
+            'nibble': 'take small bites out of',
+            'hefty': 'large and heavy',
+            'broomstick': 'a brush with twigs at one end and a long handle, on which, in children\'s literature, witches are said to fly',
+            'souvenir': 'a thing that is kept as a reminder of a person, place, or event',
+            'shrugged': 'raise (one\'s shoulders) slightly and momentarily to express doubt, ignorance, or indifference'
+        };
+
         if (glossaries != null) {
             for (var glossaryTerm in glossaries) {
                 highlightText.highlight(glossaryTerm, "highlight");
@@ -35,11 +47,15 @@
                 } else {
                     jQuery('.holder').text(glossary);
                     jQuery('.holder').show();
-                    jQuery('.holder').css({ top: e.pageY, left: e.pageX, position: 'absolute', background: '#000000' });
-                    jQuery('.holder').css({ color: '#ffffff', padding: '5px', 'border-radius': '5px' });
+                    jQuery('.holder').css({ top: e.pageY, left: e.pageX, position: 'absolute', background: '#eee' });
+                    jQuery('.holder').css({ color: '#000000', padding: '5px', 'border-radius': '5px', 'font-size': '13px', 'width': '200px' });
                 }
             }
         });
 
+        jQuery('span.highlight').on('mouseout', function (e) {
+            jQuery(this).css('cursor', 'default');
+            jQuery('.holder').hide();
+        });
     });
 })(jQuery);
