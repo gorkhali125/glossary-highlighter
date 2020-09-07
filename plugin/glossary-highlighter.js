78c12a70057fa09b9b878e6e458e9406ff9a8e89
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-09-07 21:41:45 +0545

    Restructure code

--- plugin/glossary-highlighter.js
+++ plugin/glossary-highlighter.js
@@ -1,26 +1,27 @@
-/**
- * glossary-highlighter.js
- * A Simple glossary/word highlighter plugin
- * 2020-09-02 - version 1.0.0
- * https://github.com/gorkhali125/glossary-highlighter/blob/master/glossary-highlighter.js
- * 
- * Copyright 2020 Deepesh khanal
- * Release under MIT License
- * https://opensource.org/licenses/MIT
- */
-
 (function (jQuery) {
 
-    //Main Highlight Function
-    jQuery.fn.highlight = function (str, className) {
-        if (str.search(/[\[\]?*+|{}\\()@.\n\r]/) == - 1) {
-            var regex = new RegExp(str, "gi");
-            return this.each(function () {
-                jQuery(this).html(jQuery(this).html().replace(regex, function (match) {
-                    return "<span class=\"" + className + "\">" + match + "</span>";
-                }));
-            });
-        }
-    };
+    jQuery(document).ready(function () {
 
+        //Main Highlight Function
+        jQuery.fn.highlight = function (str, className) {
+            if (str.search(/[\[\]?*+|{}\\()@.\n\r]/) == - 1) {
+                var regex = new RegExp(str, "gi");
+                return this.each(function () {
+                    jQuery(this).html(jQuery(this).html().replace(regex, function (match) {
+                        return "<span class=\"" + className + "\">" + match + "</span>";
+                    }));
+                });
+            }
+        };
+
+        var highlightText = jQuery('.body');
+        var glossaries = { 'matters': 'A substance', 'hungry': 'Empty Stomach', 'rumbling': 'Gudung Gudung', 'began': 'Started' };
+        if (glossaries != null) {
+            for (var glossaryTerm in glossaries) {
+                highlightText.highlight(glossaryTerm, "highlight");
+            }
+            jQuery('<div class="holder"></div>').appendTo('.body');
+            jQuery('span.highlight').css({ 'background-color': '#DDDCDA', 'padding': '1px', 'border-radius': '1px' });
+        }
+    });
 })(jQuery);
